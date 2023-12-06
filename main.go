e24316c5ce3c35e28ca2cbbb9a94eb2652975188
Leonardo de Faveri <faver_i@hotmail.com>
2023-12-05 21:51:14 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "hello, world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+	_ = http.ListenAndServe(":3000", nil)
+}
