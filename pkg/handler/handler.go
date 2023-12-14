db995ec3abef3913e6fa2d436b7d3c89a1ea54f3
Leonardo de Faveri <faver_i@hotmail.com>
2023-12-14 19:41:47 -0300

    refactor: building a more complex cache template

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -8,10 +8,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "home.tmpl")
+	render.Template(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "about.tmpl")
+	render.Template(w, "about.page.tmpl")
 }
