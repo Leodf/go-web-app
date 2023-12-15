7834483be662ce0c603c41e21a9c29dd341d4f67
Leonardo de Faveri <faver_i@hotmail.com>
2023-12-14 21:30:56 -0300

    add go-chi and recoverer middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com.br/Leodf/go-web-app/pkg/config"
 	"github.com.br/Leodf/go-web-app/pkg/handler"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", http.HandlerFunc(handler.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handler.Repo.About))
